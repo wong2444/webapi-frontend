92594793afe07db679997dd2695fd46050ef7097
DESKTOP-P9I81RM\wong <a26bc48@gmail.com>
2019-05-03 17:13:51 +0800

    fix some bugs

--- src/App.vue
+++ src/App.vue
@@ -93,6 +93,8 @@
         },
         mounted() {
             this.checkLogin()
+            const { NODE_ENV } = process.env;
+            document.documentElement.className = NODE_ENV;
 
         }
     }
